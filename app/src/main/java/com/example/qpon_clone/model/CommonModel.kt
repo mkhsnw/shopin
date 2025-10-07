28599c9e8dd26a9a5461424d0d8d89a2df0abd8d
Muhammad Khosyi Nawwari <stsunayoshi977@gmail.com>
2025-10-07 07:39:30 +0700

    feat:auth Complete register and login

---
+++ app/src/main/java/com/example/qpon_clone/model/CommonModel.kt
@@ -0,0 +1,41 @@
+package com.example.qpon_clone.model
+
+import com.google.gson.annotations.SerializedName
+
+data class ApiResponse<T>(
+    @SerializedName("status")
+    val status: String,
+    @SerializedName("code")
+    val code: Int,
+    @SerializedName("message")
+    val message: String,
+    @SerializedName("data")
+    val data: T,
+    @SerializedName("meta")
+    val meta: Meta? = null
+)
+
+
+data class Meta(
+    @SerializedName("page")
+    val page: Int,
+    @SerializedName("per_page")
+    val perPage: Int,
+    @SerializedName("total")
+    val total: Int,
+    @SerializedName("total_pages")
+    val totalPages: Int
+)
+
+data class Params(
+    @SerializedName("search")
+    val search: String? = null,
+    @SerializedName("latitude")
+    val latitude: String? = null,
+    @SerializedName("longitude")
+    val longitude: String? = null,
+    @SerializedName("limit")
+    val limit: Int? = null,
+    @SerializedName("page")
+    val page: Int? = null
+)
--- app/src/main/java/com/example/qpon_clone/screen/SignupScreen.kt
+++ app/src/main/java/com/example/qpon_clone/screen/SignupScreen.kt
@@ -1,5 +1,6 @@
 package com.example.qpon_clone.screen
 
+import android.widget.Toast
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -12,11 +13,13 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
@@ -25,16 +28,20 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextMotion
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
 import com.example.qpon_clone.R
 import com.example.qpon_clone.components.EmailTextField
 import com.example.qpon_clone.components.FullnameTextfield
 import com.example.qpon_clone.components.PasswordTextField
 import com.example.qpon_clone.components.PrimaryButton
+import com.example.qpon_clone.remote.Result
 import com.example.qpon_clone.view_model.AuthViewModel
 
 @Composable
-fun SignupScreen(modifier: Modifier = Modifier, navController: NavHostController, viewModel: AuthViewModel = AuthViewModel()) {
+fun SignupScreen(modifier: Modifier = Modifier, navController: NavHostController, viewModel: AuthViewModel = viewModel()) {
+
+    val context = LocalContext.current
 
     var email by remember {
         mutableStateOf("")
@@ -48,29 +55,25 @@ fun SignupScreen(modifier: Modifier = Modifier, navController: NavHostController
         mutableStateOf("")
     }
 
-//    var isLoading by remember { mutableStateOf(false) }
-//    var errorMessage by remember { mutableStateOf("") }
-//    val registerResponse = viewModel.registerResponse.observeAsState()
+    val registerResponse by viewModel.registerResponse.observeAsState()
+    val isLoading = registerResponse is Result.Loading
 
-//    LaunchedEffect(registerResponse.value) {
-//        when (registerResponse.value) {
-//            is NetworkResponse.Loading -> {
-//                isLoading = true
-//                errorMessage = ""
-//            }
-//            is NetworkResponse.Success -> {
-//                isLoading = false
-//                navController.navigate("login")
-//            }
-//            is NetworkResponse.Failed -> {
-//                isLoading = false
-//                errorMessage = (registerResponse.value as NetworkResponse.Failed).message
-//            }
-//            null -> {
-//                isLoading = false
-//            }
-//        }
-//    }
+    LaunchedEffect(registerResponse) {
+        when (registerResponse) {
+            is Result.Success -> {
+                Toast.makeText(context, "Register Success, Please Login", Toast.LENGTH_SHORT).show()
+                navController.navigate("login") {
+                    popUpTo("register") { inclusive = true }
+                }
+            }
+            is Result.Error -> {
+                val errorMessage = (registerResponse as Result.Error).exception.message
+                Toast.makeText(context, errorMessage, Toast.LENGTH_SHORT).show()
+            }
+            is Result.Loading -> {}
+            null -> {}
+        }
+    }
 
 
     Column (
@@ -101,11 +104,7 @@ fun SignupScreen(modifier: Modifier = Modifier, navController: NavHostController
         FullnameTextfield(modifier, value = fullname, onValueChange = { fullname = it}, isError = false)
         Spacer(modifier = Modifier.height(20.dp))
         PrimaryButton(modifier, "Register", {
-//            if (email.isNotBlank() && password.isNotBlank() && fullname.isNotBlank()) {
-//                viewModel.register(fullname, email, password)
-//            } else {
-//                errorMessage = "Please fill all the fields"
-//            }
+            viewModel.register(email = email, password = password, name = fullname)
         })
 //        if(errorMessage.isNotBlank()){
 //            ErrorText(modifier, errorMessage)
